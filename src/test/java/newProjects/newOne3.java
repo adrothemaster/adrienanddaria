b27638cd44c71f2295ca6ec3821432540332176a
adrothemaster <81396222+adrothemaster@users.noreply.github.com>
2021-07-16 18:15:06 -0500

    day one 10

--- src/test/java/newProjects/newOne3.java
+++ src/test/java/newProjects/newOne3.java
@@ -4,6 +4,7 @@ public class newOne3 {
     public static void main(String[] args) {
         int i = 1234;
         boolean t = true;
-
+        double m = 2.03;
+        System.out.println(i+m);
     }
 }
