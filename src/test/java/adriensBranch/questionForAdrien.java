363b7109d3b475f8837585aecaac0a402e5aa682
adrothemaster <81396222+adrothemaster@users.noreply.github.com>
2021-07-16 19:17:57 -0500

    adriens branch new one with problem

--- src/test/java/adriensBranch/questionForAdrien.java
+++ src/test/java/adriensBranch/questionForAdrien.java
@@ -2,7 +2,7 @@ package adriensBranch;
 
 public class questionForAdrien {
     public static void main(String[] args) {
-        /*
+        /**
         return true if 1001 is palindrome
          */
     }
